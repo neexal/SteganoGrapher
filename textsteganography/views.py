157b88d7d25d6bd132c2156a3a0519c8f02bcbfa
neexal <nishanghimire111@gmail.com>
2023-06-10 18:44:44 +0545

    [Routing] Complete
    Commit

--- textsteganography/views.py
+++ textsteganography/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import TextMessage
+from django.contrib.auth.decorators import login_required
+import os
 
 # Create your views here.
 def index(request):
@@ -9,17 +11,62 @@ def index(request):
 def encode(request):
     if request.method == 'POST':
         message = request.POST.get('message')
-        password = request.POST.get('password')
+        # password = request.POST.get('password')
         text_file = request.FILES.get('text_file')
         text_message = TextMessage.objects.create(user=request.user,message=message, text_file = text_file)
         text_message.save()
-        return redirect('textsteganography:download')
-    else:
-        return render(request, 'textsteganography/index.html')
-    
-def download(request):
-    return render(request,'textsteganography/download.html')
+        cover_content = text_file.read().decode('utf-8')
+        encoded_text = ''
 
+        # Convert each character in message to binary and replace LSB of each corresponding character in cover_content
+        for i, char in enumerate(message):
+            if i < len(cover_content):
+                binary_char = format(ord(char), '08b')
+                cover_char = cover_content[i]
+                binary_cover_char = format(ord(cover_char), '08b')
+                encoded_char = binary_cover_char[:-1] + binary_char[-1]  # Replace LSB of cover_char with LSB of binary_char
+                encoded_text += chr(int(encoded_char, 2))
+            else:
+                encoded_text += char
 
+            # Append remaining characters in the cover_content to the encoded text
+            encoded_text += cover_content[len(message):]
+
+            # Save encoded text to database
+            encoded_text_object = TextMessage(text_file=encoded_text, user=request.user)
+            encoded_text_object.save()
+
+            # Save updated cover file
+            text_file.seek(0)
+            text_file.write(encoded_text.encode('utf-8'))
+
+            return redirect('textsteganography:download')
+        else:
+            return render(request, 'textsteganography/index.html')
+
+@login_required
 def decode(request):
-    pass
+    if request.method == 'POST':
+        encoded_text = request.POST['encoded_text']
+        decoded_text = ''
+
+        # Extract LSB of each character in encoded_text and convert to binary
+        for char in encoded_text:
+            binary_char = format(ord(char), '08b')
+            decoded_text += binary_char[-1]  # Extract LSB
+
+        # Convert binary string to ASCII string
+        decoded_text = ''.join(chr(int(decoded_text[i:i+8], 2)) for i in range(0, len(decoded_text), 8))
+
+        encoded_text_object = TextMessage(text=encoded_text, user=request.user)
+        encoded_text_object.save()
+
+        return render(request, 'textsteganography/result.html', {'message': decoded_text})
+    else:
+        return render(request, 'textsteganography/decode.html')
+
+@login_required
+def download(request):
+    latest_text = TextMessage.objects.filter(user=request.user).latest('uploaded_at')
+    context = {'text': latest_text}
+    return render(request, 'textsteganography/download.html', context)
--- videosteganography/views.py
+++ videosteganography/views.py
@@ -1,39 +1,79 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.core.files.storage import FileSystemStorage
+from moviepy.editor import VideoFileClip
 from .models import VideoMessage
 
 
-# Create your views here.
 def index(request):
-
     # if user is not logged in, redirect to home page
     if not request.user.is_authenticated:
         return redirect('accounts:login')
-
     return render(request, 'videosteganography/index.html')
 
 
 @login_required
 def encode(request):
-    if request.method == 'POST':
+    if request.method == 'POST' and request.FILES['video_file']:
+        video_file = request.FILES['video_file']
         message = request.POST.get('message')
-        video_file = request.FILES.get('video_file')
-        # attach current user to video
-        video = VideoMessage(
-            message=message, video_file=video_file, user=request.user)
-        video.save()
-        return redirect('videosteganography:download')
-    else:
-        return render(request, 'videosteganography/index.html')
-
+        video_message = VideoMessage.objects.create(user=request.user,message=message, video_file = video_file)
+        video_message.save()
+        fs = FileSystemStorage()
+        filename = fs.save(video_file.name, video_file)
+        video_path = fs.path(filename)
+        clip = VideoFileClip(video_path)
+        audio = clip.audio
+        binary_message = ''.join(format(ord(i), '08b') for i in message)
+        binary_message += "00000000"
+        binary_message_index = 0
+        audio_length = len(audio)
+        for i in range(audio_length):
+            if binary_message_index == len(binary_message):
+                break
+            binary_frame = format(audio[i], '016b')[:-2] + binary_message[binary_message_index:binary_message_index+2]
+            audio[i] = int(binary_frame, 2)
+            binary_message_index += 2
+        new_clip = clip.set_audio(audio)
+        new_clip.write_videofile(filename, codec='libx264')
+        with open(filename, 'rb') as f:
+            video_data = f.read()
+        video_message = VideoMessage()
+        video_message.filename = filename
+        video_message.video_data = video_data
+        video_message.save()
+        return redirect('download', video_message_id=video_message.id)
+    return render(request, 'videosteganography/index.html')
 
 @login_required
-def download(request):
-    return render(request, 'videosteganography/download.html')
+def download(request, video_message_id):
+    video_message = VideoMessage.objects.get(id=video_message_id)
+    with open(video_message.filename, 'wb') as f:
+        f.write(video_message.video_data)
+    video_path = video_message.filename
+    return render(request, 'videosteganography/download.html', {'video_path': video_path})
+
+def decode(request, video_message_id):
+    video_message = VideoMessage.objects.get(id=video_message_id)
+    with open(video_message.filename, 'wb') as f:
+        f.write(video_message.video_data)
+    clip = VideoFileClip(video_message.filename)
+    audio = clip.audio
+    binary_message = ""
+    audio_length = len(audio)
+    for i in range(audio_length):
+        binary_frame = format(audio[i], '016b')[-2:]
+        binary_message += binary_frame
+        if binary_message[-8:] == "00000000":
+            break
+    message = ""    
+    for i in range(0, len(binary_message), 8):
+        message += chr(int(binary_message[i:i+8], 2))
+        if message[-1] == "\x00":
+            break
+    return render(request, 'videosteganography/result.html', {'message': message})
 
 
-def decode(request):
-    pass
 
 # def encode(request):
 #     user = request.user
